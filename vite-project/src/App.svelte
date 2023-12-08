3e68f8b6cf422595cf26ec3fd7d01847fc61c654
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2023-12-08 11:01:26 +0200

    My first svelte page

---
+++ vite-project/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Pointer from './lib/Pointer.svelte'
+</script>
+
+<style>
+  h1{
+   color:#FF3E00;
+  }
+ </style>
+
+<h1>My first Svelte build</h1>
+
+<Pointer  />
---
+++ vite-project/src/lib/Pointer.svelte
@@ -0,0 +1,20 @@
+<script>
+    let mousePosition = { x: 0, y: 0 };
+  
+    const handleMouseMove = (e) => {
+      mousePosition = { x: e.clientX, y: e.clientY };
+    };
+  </script>
+  
+  <style>
+   h1{
+    color:#FF3E00;
+   }
+  </style>
+  
+  
+  <svelte:window on:mousemove={handleMouseMove} />
+
+  <h1>The mouse position is at x:{mousePosition.x} y:{mousePosition.y}</h1>
+  
+  
